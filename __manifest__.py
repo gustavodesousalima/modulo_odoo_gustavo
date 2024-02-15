92bfeeb95152436388bb19bd285299baea47dd18
gustavodesousalima <gustavodesousalima918@gmail.com>
2024-02-15 17:35:22 -0300

    alter module

--- __manifest__.py
+++ __manifest__.py
@@ -6,6 +6,8 @@
     'author': 'Gustavo de Sousa Lima',
     'data': [
         'security/ir.model.access.csv',
+        "report/modulo_odoo_report_template.xml",
+        "report/modulo_odoo_report.xml",
         'views/modulo_odoo_gustavo.xml',
     ],
     'installable': True,
